52ccdbe64776400b48b3cb2ae938f25427d7ba4c
Alexandre Vinet <alexandre.vinet@sensiolabs.com>
2018-03-20 17:35:45 +0100

    [ch3191] Catch errors from mailer client
    https://app.clubhouse.io/enmarche/story/3191/bug-lorsque-l-api-de-mailjet-renvoie-un-code-r%C3%A9ponse-4xx-une-exception-non-g%C3%A9r%C3%A9e-est-lev%C3%A9e-ce-qui-fait-planter-les-workers

--- src/Mailjet/EmailClient.php
+++ src/Mailjet/EmailClient.php
@@ -5,15 +5,30 @@ namespace AppBundle\Mailjet;
 use AppBundle\Mailer\AbstractEmailClient;
 use AppBundle\Mailer\EmailClientInterface;
 use AppBundle\Mailer\Exception\MailerException;
+use GuzzleHttp\ClientInterface as Guzzle;
+use GuzzleHttp\Exception\ClientException;
+use Monolog\Logger;
 use Symfony\Component\HttpFoundation\Response;
 
 class EmailClient extends AbstractEmailClient implements EmailClientInterface
 {
+    private $logger;
+
+    public function __construct(Guzzle $httpClient, string $publicKey, string $privateKey, Logger $logger)
+    {
+        parent::__construct($httpClient, $publicKey, $privateKey);
+        $this->logger = $logger;
+    }
+
     public function sendEmail(string $email): string
     {
-        $response = $this->request('POST', 'send', [
-            'body' => $email,
-        ]);
+        try {
+            $response = $this->request('POST', 'send', ['body' => $email]);
+        } catch (ClientException $clientException) {
+            $this->logger->error('Client error', $clientException);
+
+            return '';
+        }
 
         if (Response::HTTP_OK !== $response->getStatusCode()) {
             throw new MailerException('Unable to send email to recipients.');
